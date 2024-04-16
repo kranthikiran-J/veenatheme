2efa60bf4e683e8cbaff83d1ec270a49cb926475
kranthikiran-J <kjavvaji@marmeto.com>
2024-04-16 11:08:35 +0530

    base.css updated for box-shadow

--- assets/featured-custom-collection.js
+++ assets/featured-custom-collection.js
@@ -1,6 +1,7 @@
 
 const radio = document.querySelectorAll('.radio')
 const firstImage = document.querySelectorAll('.get-img')
+console.log(firstImage)
 console.log(radio)
 
 radio.forEach((eachradio)=>{
@@ -8,6 +9,22 @@ radio.forEach((eachradio)=>{
         let imageLink = eachradio.getAttribute("data-images");
         let imageElIndex = eachradio.getAttribute("data-imageindex") - 1;
         firstImage[imageElIndex].setAttribute("src",imageLink);
+        console.log(eachradio);
+    })
+})
+
+
+
+const radio1 = document.querySelectorAll('.radio-1')
+const firstImage1 = document.querySelectorAll('.get-img-2')
+console.log(firstImage)
+console.log(radio)
+
+radio1.forEach((eachradio)=>{
+    eachradio.addEventListener("mouseenter",function(){
+        let imageLink = eachradio.getAttribute("data-images");
+        let imageElIndex = eachradio.getAttribute("data-imageindex") - 1;
+        firstImage1[imageElIndex].setAttribute("src",imageLink);
         console.log(eachradio);
     })
 })
